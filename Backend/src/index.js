4efb4b2b0777f940eec692cd56810ecdab2c6144
Abhiram2523 <abhiram2523@gmail.com>
2025-02-22 12:47:02 +0530

    completed 4

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,20 +1,19 @@
+import cors from "cors";
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors";
-
-import path from "path";
-
-import { connectDB } from "./lib/db.js";
-
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
 const __dirname = path.resolve();
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
@@ -27,16 +26,18 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
-  });
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 }
 
+
 server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
-  connectDB();
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,29 +1,35 @@
-import e from "express";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-    {
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        fullName: {
-            type: String,
-            required: true,
-        },
-        password: {
-            type: String,
-            required: true,
-            minlength: 6,
-        },
-        profilePic: {
-            type: String,
-            default: "",
-        },
-    },
-    { timestamps: true }
+	{
+		fullName: {
+			type: String,
+			required: true,
+		},
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			minlength: 6,
+		},
+		gender: {
+			type: String,
+			required: true,
+			enum: ["male", "female"],
+		},
+		profilePic: {
+			type: String,
+			default: "",
+		},
+		// createdAt, updatedAt => Member since <createdAt>
+	},
+	{ timestamps: true }
 );
 
-export default mongoose.model("User", userSchema);
-    
+const User = mongoose.model("User", userSchema);
+
+export default User;
