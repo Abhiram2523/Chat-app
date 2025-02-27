116e25a537bcb0fa28fa834e72ba261f769de5cd
Abhiram2523 <abhiram2523@gmail.com>
2025-02-27 17:29:10 +0530

    completed 8

--- Frontend/src/components/AuthImagePattern.jsx
+++ Frontend/src/components/AuthImagePattern.jsx
@@ -1,19 +1,23 @@
+import chatImage from "../assets/chat.png";
 
 const AuthImagePattern = ({ title, subtitle }) => {
-    return (
-      <div
-  className="hidden lg:flex items-center justify-center bg-base-200 p-12"
-  style={{ backgroundImage: "url(/chat.png)", backgroundSize: "cover", backgroundPosition: "center" }}
->
-  <div className="max-w-md text-center">
-    <div>
-      
+  return (
+    <div
+      className="hidden lg:flex items-center justify-center bg-base-200 p-12"
+      style={{
+        backgroundImage: `url(${chatImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="max-w-md text-center">
+        <h2 className="text-2xl font-bold mb-4">{title}</h2>
+        <p className="text-base-content/60 drop-shadow-[0_0_6px_white]">
+          {subtitle}
+        </p>
+      </div>
     </div>
-    <h2 className="text-2xl font-bold mb-4">{title}</h2>
-    <p className="text-base-content/60 drop-shadow-[0_0_6px_white]">{subtitle}</p>
-  </div>
-</div>
-    );
-  };
-  
-  export default AuthImagePattern;
+  );
+};
+
+export default AuthImagePattern;
